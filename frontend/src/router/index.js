4ee0894a053bf3c5976fcf5a70f526f6f8c7bae8
lin ny <12637836+lin-ny@user.noreply.gitee.com>
2024-06-03 19:44:33 +0800

    Revert "change routes"
    This reverts commit 58593d0893949ed2914b34e5611de90dd19f8835.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-//  routes: [
-//    {
-//      path: '*',
-//      redirect: () => {
-//        return '/index'
-//      }
-//    },
-//    {
-//      path: '/index',
-//      component: () => import('@views/index')
-//    }
-//  ]
   routes: [
     {
-      path: '/',
-      // name: 'HelloWorld',
-      name: 'HelloWorld',
-      // component: HelloWorld
-      component: HelloWorld
+      path: '*',
+      redirect: () => {
+        return '/index'
+      }
+    },
+    {
+      path: '/index',
+      component: () => import('@views/index')
     }
   ]
 })
