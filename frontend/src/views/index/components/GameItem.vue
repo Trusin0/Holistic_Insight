5dc5121bb2436d889e3f7e082ae23b46f4aafbe7
Trusin Catherine <zhengbch5@mail2.sysu.edu.cn>
2024-06-12 16:52:58 +0800

    修改了界面和README.md，可以开始向view中提交代码

---
+++ frontend/src/views/index/components/GameItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="game-item" @click="$router.push(path)">
+    <h4>{{ title }}</h4>
+    <span>{{ intro }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    intro: {
+      type: String,
+      default: ''
+    },
+    path: {
+      type: String,
+      default: '/index'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.game-item {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 260px;
+  height: 260px;
+  color: #333;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* 替换为具体的阴影样式 */
+  border-radius: 10px;
+  margin: 20px auto 0 auto;
+  cursor: pointer;
+  top: 0;
+  transition: transform .5s;
+}
+
+.game-item:hover {
+  transform: translate(0, -10px);
+}
+
+.game-item h4 {
+  font-size: 24px;
+}
+
+.game-item span {
+  font-size: 18px;
+}
+
+@media screen and (max-width: 480px) {
+  .game-item {
+    width: 100%;
+    border-radius: 0; /* 取消边框半径 */
+  }
+}
+
+</style>
---
+++ frontend/src/views/index/config/GameList.js
@@ -0,0 +1,42 @@
+export const gameList = [
+  {
+    id: 1,
+    title: '反应时间',
+    intro: '测试您的视觉反应',
+    path: '/reaction-time',
+    best: 1, // 最佳成绩取最大值/最小值，1 取最小值，-1取最大值
+    section: 25, // 取值区块间隔
+    scoreRange: [100, 500], // 成绩取值范围
+    unit: 'ms'
+  },
+  {
+    id: 2,
+    title: '数字记忆',
+    intro: '测试您的数字记忆能力',
+    path: '/number-memory'
+  },
+  {
+    id: 3,
+    title: '测试用',
+    intro: '测试用',
+    path: '/test'
+  },
+  {
+    id: 4,
+    title: '测试用',
+    intro: '测试用',
+    path: '/test'
+  },
+  {
+    id: 5,
+    title: '测试用',
+    intro: '测试用',
+    path: '/test'
+  },
+  {
+    id: 6,
+    title: '测试用',
+    intro: '测试用',
+    path: '/test'
+  }
+]
